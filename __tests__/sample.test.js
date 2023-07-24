3bab0e46595cb2434eebea90cdc08cba3811735d
John Dazey <jfpdazey@gmail.com>
2023-07-24 19:35:50 -0400

    first commit of basic framework

---
+++ __tests__/sample.test.js
@@ -0,0 +1,7 @@
+//import { add } from "../src/sample";
+
+describe("First case", () => {
+    it("tests something", () => {
+        expect(true).toEqual(false);      
+    })
+});
---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+const config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  rootDir: '.',
+};
+
+module.exports = config;
